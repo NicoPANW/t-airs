80fbdb3c8231d2c705cbb96343dfd4150d32b7df
NicoPANW <40418462+NicoPANW@users.noreply.github.com>
2026-04-09 13:41:54 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,7 +11,7 @@ output "deployment_summary" {
   💻 SSH Command     : ${var.target_cloud == "gcp" ? "gcloud compute ssh ${try(google_compute_instance.t_airs_node[0].name, "")} --zone=${try(google_compute_instance.t_airs_node[0].zone, "")}" : "ssh ubuntu@${try(aws_instance.t_airs_node[0].public_ip, "")}"}
   
   🛡️ Security Notice : CSP firewalls are RESTRICTED to your auto-detected subnet
-                       (${local.my_auto_subnet}) and Prisma AIRS nodes.
+                       (${local.my_auto_subnet}) and Prisma AIRS nodes (${join(", ", var.prisma_airs_ips)}).
   
   ⏳ Readiness Note  : Please note it takes approximately 10 minutes for the startup 
                        scripts to finish installing the application. The dashboard 
