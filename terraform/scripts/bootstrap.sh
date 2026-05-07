251fcb6c6d78ef01af6c0f14812cf4434ff14961
NicoPANW <40418462+NicoPANW@users.noreply.github.com>
2026-05-07 16:35:32 +0200

    Update bootstrap.sh

--- terraform/scripts/bootstrap.sh
+++ terraform/scripts/bootstrap.sh
@@ -127,16 +127,6 @@ echo "Installing remaining Python requirements..."
 pip3 install -r /opt/t-airs/src/requirements.txt
 
 
-if [ "${target_cloud}" == "aws" ]; then
-    echo "Applying AWS network fixes (MTU 1500 & Disable IPv6)..."
-    # Fix Jumbo Frame Blackhole
-    PRIMARY_IF=$(ip route show default | awk '/default/ {print $5}')
-    ip link set dev $PRIMARY_IF mtu 1500
-    # Fix IPv6 Handshake Timeouts
-    sysctl -w net.ipv6.conf.all.disable_ipv6=1
-    sysctl -w net.ipv6.conf.default.disable_ipv6=1
-fi
-
 echo "Pre-downloading HuggingFace BAAI Embedding Model..."
 # We run this in the foreground so systemd doesn't choke the network connection!
 python3 -c "from huggingface_hub import snapshot_download; snapshot_download('BAAI/bge-base-en-v1.5')"
